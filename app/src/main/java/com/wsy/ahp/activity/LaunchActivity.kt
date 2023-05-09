6ad54ab627598d3d4d79adde965a8f3b8749cc19
npkj1803 <13136269009@163.com>
2023-05-09 20:31:23 +0800

    优化状态栏颜色展示，半透明，透明，隐藏

--- app/src/main/java/com/wsy/ahp/activity/LaunchActivity.kt
+++ app/src/main/java/com/wsy/ahp/activity/LaunchActivity.kt
@@ -1,14 +1,14 @@
 package com.wsy.ahp.activity
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.wsy.ahp.MainActivity
 import com.wsy.ahp.R
 import com.wsy.ahp.adapter.LaunchImproveAdapter
 import com.wsy.ahp.http.common.ArouterUrl
 import com.wsy.common.ui.component.HiBaseActivity
-import com.wsy.wsy_library.util.HiStatusBar.setStatusBar
+import com.wsy.common.utils.StatusBar
 
 @Route(path = ArouterUrl.HOME_ANSWER)
 class LaunchActivity: HiBaseActivity() {
@@ -19,8 +19,18 @@ class LaunchActivity: HiBaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setStatusBar(this, true, Color.TRANSPARENT, false)
+
+        val statusBar =  StatusBar(this@LaunchActivity);
+        //设置颜色为半透明
+        statusBar.setColor(R.color.translucent);
+//        //设置颜色为透明
+//        statusBar.setColor(R.color.transparent);
+//        //隐藏状态栏
+//        statusBar.hide();
+
+
         setContentView(R.layout.activity_launch_improve)
+
         val vp_launch: ViewPager = findViewById<ViewPager>(R.id.vp_launch)
         val adapter = LaunchImproveAdapter(supportFragmentManager, lanuchImageArray)
         vp_launch.adapter = adapter
