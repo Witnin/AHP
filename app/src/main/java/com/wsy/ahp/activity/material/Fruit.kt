5f0cadc54b9cdd5dc56a0b16f01f865e52a18e44
npkj1803 <13136269009@163.com>
2023-06-06 13:54:51 +0800

    配置MaterialCardView卡片布局，、RecyclerView+实现AppBarLayout向下滚动隐藏

---
+++ app/src/main/java/com/wsy/ahp/activity/material/Fruit.kt
@@ -0,0 +1,5 @@
+package com.wsy.ahp.activity.material
+
+class Fruit(val name: String, val imageId: Int)
+{
+}
--- app/src/main/java/com/wsy/ahp/activity/material/TestMaterialActivity.kt
+++ app/src/main/java/com/wsy/ahp/activity/material/TestMaterialActivity.kt
@@ -1,11 +1,14 @@
 package com.wsy.ahp.activity.material
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.google.android.material.snackbar.Snackbar
 import com.wsy.ahp.R
@@ -13,10 +16,20 @@ import com.wsy.ahp.http.common.ArouterUrl
 import kotlinx.android.synthetic.main.activity_login.view
 import kotlinx.android.synthetic.main.activity_test_material.drawerLayout
 import kotlinx.android.synthetic.main.activity_test_material.fab
+import kotlinx.android.synthetic.main.activity_test_material.material_recyclerView
 import kotlinx.android.synthetic.main.activity_test_material.toolbar
 
 @Route(path = ArouterUrl.MATERIAL_TEST)
 class TestMaterialActivity : AppCompatActivity() {
+
+    val fruits = mutableListOf(Fruit("Apple", R.drawable.apple), Fruit("Banana",
+        R.drawable.banana), Fruit("Orange", R.drawable.orange), Fruit("Watermelon",
+        R.drawable.watermelon), Fruit("Pear", R.drawable.pear), Fruit("Grape",
+        R.drawable.grape), Fruit("Pineapple", R.drawable.pineapple), Fruit("Strawberry",
+        R.drawable.strawberry), Fruit("Cherry", R.drawable.cherry), Fruit("Mango",
+        R.drawable.mango))
+    val fruitList = ArrayList<Fruit>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_material)
@@ -34,12 +47,27 @@ class TestMaterialActivity : AppCompatActivity() {
                 .show()
         }
 
+        initFruits()
+        val layoutManager = GridLayoutManager(this, 2)
+        material_recyclerView.layoutManager = layoutManager
+        val adapter = FruitAdapter(this, fruitList)
+        material_recyclerView.adapter = adapter
+
+    }
+
+    private fun initFruits() {
+        fruitList.clear()
+        repeat(50) {
+            val index = (0 until fruits.size).random()
+            fruitList.add(fruits[index])
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar, menu)
         return true
     }
+    @SuppressLint("RtlHardcoded")
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.backup -> Toast.makeText(this, "You clicked Backup",
